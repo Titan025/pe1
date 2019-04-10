3ac376d9a9192ef9d80107ac1b12f9e98e58076d
Titan025 <madtiw25@gmail.com>
2019-04-10 11:29:04 +0530

    welcome to pozitron

---
+++ models/enquiry.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Enquiry = new Schema(
+  {name: String,
+    email:String,
+    subject: String,
+    message: String,
+    
+}
+
+);
+mongoose.model('enqs',Enquiry);
